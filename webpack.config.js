d07745235f8fa17ba8d44a4acf98828b552f5256
Collin <ococncol@gmail.com>
2020-09-01 20:06:14 -0400

    still need to get js working

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,15 @@
 const path = require('path')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+// const VueLoaderPlugin = require('vue-loader/lib/plugin')
 
 module.exports = {
-    entry: [
-        './src/js/main.js',
-        './src/sass/main.sass'
-    ],
+    entry: {
+        main: './src/js/main.js',
+        main: './src/sass/main.sass'
+    },
     output: {
         filename: 'js/[name].js',
+        chunkFilename: '[name].bundle.js',
         path: path.resolve(__dirname, 'public'),
         publicPath: '/public/'
     },
@@ -16,6 +18,7 @@ module.exports = {
             {
                 test: /\.(sa|sc|c)ss$/,
                 use: [
+                    'vue-style-loader',
                     {
                         loader: MiniCssExtractPlugin.loader,
                         options: {
@@ -25,12 +28,57 @@ module.exports = {
                     'css-loader',
                     'sass-loader',
                 ]
-            }
+            },
+            // {
+            //     test: /\.vue$/,
+            //     loader: 'vue-loader'
+            // },
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/
+              },
+            {
+                test: /\.(png|jpg|gif|svg)$/,
+                loader: 'file-loader',
+                options: {
+                  name: '[name].[ext]?[hash]'
+                }
+              }
         ]
     },
+    // resolve: {
+    //     alias: {
+    //       'vue$': 'vue/dist/vue.esm.js'
+    //     },
+    //     extensions: ['*', '.js', '.vue', '.json']
+    //   },
     plugins: [
+        // new VueLoaderPlugin(),
         new MiniCssExtractPlugin({
             filename: 'css/[name].css',
-        })
-    ]
+        }),
+    ],
+    devtool: '#eval-source-map',
 }
+
+if (process.env.NODE_ENV === 'production') {
+    module.exports.devtool = '#source-map'
+    // http://vue-loader.vuejs.org/en/workflow/production.html
+    module.exports.plugins = (module.exports.plugins || []).concat([
+      new webpack.DefinePlugin({
+        'process.env': {
+          NODE_ENV: '"production"'
+        }
+      }),
+      new webpack.optimize.UglifyJsPlugin({
+        sourceMap: true,
+        compress: {
+          warnings: false
+        }
+      }),
+      new webpack.LoaderOptionsPlugin({
+        minimize: true
+      })
+    ])
+  }
